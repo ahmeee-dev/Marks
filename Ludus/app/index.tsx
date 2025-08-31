09a9828c2b521bad6bae0640fba56110f00a2cdd
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-08-31 22:46:14 +0200

    feat: added HomePage strucure

--- Ludus/app/index.tsx
+++ Ludus/app/index.tsx
@@ -1,29 +1,60 @@
-import { View, Text, StyleSheet  } from "react-native";
-import { VELOCITY_EPS } from "react-native-reanimated/lib/typescript/animation/decay/utils";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { View, Text, TextInput, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from "react-native";
+import React from 'react';
+import ScrollElements from "../components/ui/ArcList";
+import ArcList from "../components/ui/ArcList";
 
-const SupportButton = () => null;
-const ProfileButton = () => null;
-const RandomText = () => null;
-const ArgumentPlaceHolder = () => null;
-const QualityScroller = () => null;
-const StartButton = () => null;
+const name = "Ludovice";
+
+const defaultExamples = [ "La caduta di Roma",
+					"Scoperta dell'America",
+					"La borghesia nella Rivoluzione francese",
+					"Invenzione della stampa",
+					"La vita di Galileo Galilei",
+					"Rinascimento italiano",
+					"Prima guerra mondiale",
+					"Seconda guerra mondiale",
+					"La Guerra fredda",
+					"Grande Depressione e New Deal"];
+
+const texts = [ `Ciao ${name}, da dove iniziamo oggi?`,
+	 		`Buongiorno ${name}, cosa vogliamo ripassare?`,
+			`Ciao ${name}, quale argomento affrontiamo?`,
+			`Ehi ${name}, cosa rivediamo insieme?`,
+			`Buongiorno ${name}, quale lezione tocchiamo oggi?`,
+			`Ciao ${name}, cosa ti va di studiare ora?`,
+			`Buongiorno ${name}, quale tema apriamo?`,
+			`Ehi ${name}, cosa mi spieghi oggi?`,
+			`Ciao ${name}, quale parte riprendiamo?`,
+			`Buongiorno ${name}, da cosa partiamo oggi?`];
 
 export default function Index() {
+	const [argument, setArgument] = useState("");
 	return (
-		<View>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+		<View style={styleSheet.mainView}>
 			<View>
-				<SupportButton/>
-				<ProfileButton/>
+				<Button title="Support"/>
+				<Button title="Profile"/>
 			</View>
 			<View>
-				<RandomText/>
-				<ArgumentPlaceHolder/>
+				<Text>{texts[Math.floor(Math.random() * texts.length)]}</Text>
+				<TextInput onEndEditing={(e) => setArgument(e.nativeEvent.text)} 
+				placeholder={`Es. + ${defaultExamples[Math.floor(Math.random() * defaultExamples.length)]}`}
+				placeholderTextColor="red"
+				/>
 			</View>
 			<View>
-				<QualityScroller/>
-				<StartButton/>
+				<ArcList/>
+				<Button title="Start"/>
 			</View>
 		</View>
-		)
-}
+		</TouchableWithoutFeedback>
+	)}
+
+const styleSheet = StyleSheet.create({
+	mainView: {
+		height: "100%",
+		justifyContent: "space-evenly",
+	},
+})
