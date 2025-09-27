7df764802bd66dfb913eaacec127c01d4ea048d9
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-09-27 12:45:44 +0200

    feat: playing with libraries

---
+++ backend/web-serv/src/main/java/com/ahmeee_dev/web_serv/WebServApplication.java
@@ -0,0 +1,13 @@
+package com.ahmeee_dev.web_serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServApplication.class, args);
+	}
+
+}
---
+++ backend/web-serv/src/test/java/com/ahmeee_dev/web_serv/WebServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmeee_dev.web_serv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
